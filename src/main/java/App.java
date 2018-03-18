f0d3f47f69b12f0ab3c19e1eba5d218e3ea85084
zexuswolf <zexuswolf@users.noreply.github.com>
2018-03-18 20:38:11 +0700

    Add test()

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,4 +9,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
+    
+    public void test(){
+        //TODO   
+    }
 }
